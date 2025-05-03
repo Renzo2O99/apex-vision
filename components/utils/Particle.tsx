ffdb7b301ccc9cc147b6cf8cfddfe4ccdb0db178
Renzo2099 <aliendrorenzo4@gmail.com>
2025-05-03 20:57:14 -0300

    #fix Particle

--- components/utils/Particle.tsx
+++ components/utils/Particle.tsx
@@ -9,6 +9,7 @@ import {
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import options from "@/lib/particleConfig";
+import FadeInOut from "./FadeInOut";
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 export const Particle = () => {
@@ -36,11 +37,13 @@ export const Particle = () => {
 
   if (init) {
     return (
-      <Particles
-        id="tsparticles"
-        particlesLoaded={particlesLoaded}
-        options={options}
-      />
+      <FadeInOut show={show} duration={500}>
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+      </FadeInOut>
     );
   }
 
